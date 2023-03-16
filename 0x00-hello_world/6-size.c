8999e5498eed2a15615f03e608c9888f62f999c8
Ilyasblk <belkacemilyas8@gmail.com@example.com>
2023-03-16 15:00:20 -0700

    hh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* You should produce the exact same output as in the example
+* Warnings are allowed
+*
+* Your program should return 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %1d byte(s)\n", sizeof(s));
+	printf("size of a int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long: %1d byte(s)\n", sizeof(li));
+	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
+	printf("size of a float: %1d byte(s)\n", sizeof(f));
+}
